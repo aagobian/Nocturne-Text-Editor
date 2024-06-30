3ac3eeee1d871726856175892949ad0d5b131c70
AAgobian <areesagob@gmail.com>
2024-06-30 00:15:58 -0400

    Add inspect element

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const path = require('path');
-const {app, BrowserWindow} = require('electron');
+const path = require('path'); 
+const {app, BrowserWindow, Menu} = require('electron');
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Nocturne',
         width: 800,
         height: 600,
+
     });
 
     mainWindow.loadFile(path.join(__dirname, './render/index.html'));
+    
+    const contextMenu = Menu.buildFromTemplate([
+        {
+          label: 'Inspect Element',
+          click: () => {
+            mainWindow.webContents.openDevTools({ mode: 'detach' });
+          },
+        },
+      ]);
+      
+        mainWindow.webContents.on('context-menu', (event) => {
+        event.preventDefault();
+        contextMenu.popup();
+      });
 }
 
 app.whenReady().then(() => {
