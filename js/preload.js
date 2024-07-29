91e9f93a325c8f607d7785641199a7e85a3cedc2
AAgobian <areesagob@gmail.com>
2024-07-29 00:07:16 -0400

    Delete files to start React migration

--- js/preload.js
+++
@@ -1,6 +0,0 @@
-// exposes connection to ipc
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  controlWindow: (action) => ipcRenderer.send('window-control', action),
-});
