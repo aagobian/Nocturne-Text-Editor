d99de000ec888409a1dcfb57de6b3085469bb36f
Samir Dawar <samir.dawar01@gmail.com>
2024-07-29 12:02:20 -0400

    test

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import react from 'react';
+
+
+const Navbar = () => {
+  return(
+    <div>
+      <h1 className='main-text'>
+        test
+      </h1>
+    </div>
+  )
+}
+
+
+export default Navbar
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,10 +2,13 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import '../components/Sidebar'
 import Sidebar from '../components/Sidebar';
+import Navbar from '../components/Navbar';
+
 function Main() {
   return (
     <div>
       <Sidebar/>
+      <Navbar/>
     </div>
   );
 }
